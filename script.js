9ce953cabdb9aad760adde86b79309a975e079ae
YevhenTkalenko <mr.undesired@gmail.com>
2023-10-21 13:26:54 +0300

    add reset btn

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-// const cityName = prompt('Введите город')
-
+const cityName = prompt('Choose city')
+const resetBtn = document.querySelector('.weather_btn')
 
 const getFetch = async () => {
-    let dataFetch = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=5d066958a60d315387d9492393935c19`);
+    let dataFetch = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=5d066958a60d315387d9492393935c19`);
     return dataFetch.json()
 }
 
+//Fetch data about weather and use IIFN
 const insertWeatherData = (async () => {
-    let inserData = await getFetch()
-    document.querySelector('.weather_city').innerHTML = `${inserData.name}, ${inserData.sys.country}`;
-    document.querySelector('.weather_temp').innerHTML = inserData.main.temp + `&deg`;
-    document.querySelector('.weather_description').textContent = inserData.weather[0].description;
-    document.querySelector('.weather_icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${inserData.weather[0].icon}@2x.png">`;
-    document.querySelector('.weather_btn a').setAttribute('href', `https://en.wikipedia.org/wiki/London`)
+    let insertData = await getFetch()
+    document.querySelector('.weather_city').innerHTML = `${insertData.name}, ${insertData.sys.country}`;
+    document.querySelector('.weather_temp').innerHTML = insertData.main.temp + `&deg`;
+    document.querySelector('.weather_description').textContent = insertData.weather[0].description;
+    document.querySelector('.weather_icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${insertData.weather[0].icon}@2x.png">`;
+    document.querySelector('.wiki_btn a').setAttribute('href', `https://en.wikipedia.org/wiki/${cityName}`)
 })()
+
+//Repeatly fetch data about weather and use eventListener
+const resetWeatherData = async () => {
+    let resetCity = prompt('Choose new city')
+    const getResetFetch = async () => {
+        let dataFetch = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${resetCity}&units=metric&APPID=5d066958a60d315387d9492393935c19`);
+        return dataFetch.json()
+    }
+    let insertData = await getResetFetch()
+    document.querySelector('.weather_city').innerHTML = `${insertData.name}, ${insertData.sys.country}`;
+    document.querySelector('.weather_temp').innerHTML = insertData.main.temp + `&deg`;
+    document.querySelector('.weather_description').textContent = insertData.weather[0].description;
+    document.querySelector('.weather_icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${insertData.weather[0].icon}@2x.png">`;
+    document.querySelector('.wiki_btn a').setAttribute('href', `https://en.wikipedia.org/wiki/${resetCity}`)
+}
+resetBtn.addEventListener('click', resetWeatherData)
+
 
 
 
