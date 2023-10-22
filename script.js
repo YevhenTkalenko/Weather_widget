a06d213b0cd8f0c930de0bc4f43a90893e728900
YevhenTkalenko <mr.undesired@gmail.com>
2023-10-22 23:52:12 +0300

    add input search and new interface

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-// const cityName = prompt('Choose city')
-const resetBtn = document.querySelector('.weather_btn')
+const weatherRef = `https://api.openweathermap.org/data/2.5/weather?q=`;
+const appiKey = `&units=metric&APPID=5d066958a60d315387d9492393935c19`
+const inputCityName = document.querySelector('.search_input');
+const searchCityBtn = document.querySelector('.search_btn');
 
-const getFetch = async () => {
-    let dataFetch = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Lviv&units=metric&APPID=5d066958a60d315387d9492393935c19`);
-    return dataFetch.json()
+
+const getFetch = async (city) => {
+    let dataFetch = await fetch(`${weatherRef}${city}${appiKey}`);
+    return insertWeatherData(dataFetch.json())
 }
 
-//Fetch data about weather and use IIFN
-const insertWeatherData = (async () => {
-    let insertData = await getFetch()
-    document.querySelector('.weather_city').innerHTML = `${insertData.name}, ${insertData.sys.country}`;
-    document.querySelector('.weather_temp').innerHTML = insertData.main.temp + `&deg`;
-    document.querySelector('.weather_description').textContent = insertData.weather[0].description;
-    document.querySelector('.weather_icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${insertData.weather[0].icon}@2x.png">`;
-    document.querySelector('.wiki_btn a').setAttribute('href', `https://en.wikipedia.org/wiki/Lviv`)
-})()
 
+async function insertWeatherData(weatherdata) {
+    let insertData = await weatherdata
+    if (insertData.cod === 200) {
+        console.log(insertData)
+        document.querySelector('.weather_city').innerHTML = `${insertData.name}, ${insertData.sys.country}`;
+        document.querySelector('.weather_temp').innerHTML = Math.round(insertData.main.temp) + `&degC`;
+        document.querySelector('.weather_description').textContent = insertData.weather[0].description;
+        document.querySelector('.weather_icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${insertData.weather[0].icon}@2x.png">`;
+        document.querySelector('.wind_speed').innerHTML = `${Math.round(insertData.wind.speed)}m/s`
+        document.querySelector('.humidity').innerHTML = `${Math.round(insertData.main.humidity)}%`
+        document.querySelector('.weather-info').style.display = 'block';
+        document.querySelector('.error_data').style.display = 'none'
+    } else if (insertData.cod !== 200) {
+        document.querySelector('.error_data').style.display = 'block'
+    }
+}
+
+
+
+searchCityBtn.addEventListener("click", () => {
+    getFetch(inputCityName.value)
+})
 
 
 
