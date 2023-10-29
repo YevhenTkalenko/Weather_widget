d7ebe21e67917ccc7f8e3a0db7ddfb85d9c701c2
YevhenTkalenko <mr.undesired@gmail.com>
2023-10-29 16:03:34 +0200

    add new styles

--- srcipts/script.js
+++ srcipts/script.js
@@ -12,21 +12,27 @@ const getFetch = async (city) => {
 }
 
 
-async function insertWeatherData(weatherdata) {
-    let insertData = await weatherdata
+async function insertWeatherData(cityName) {
+    let insertData = await cityName;
+    let weatherCartIcon = document.querySelector('.weather_icon img');
     if (insertData.cod === 200) {
         console.log(insertData)
+        weatherCartIcon.setAttribute('src', `https://openweathermap.org/img/wn/${insertData.weather[0].icon}@2x.png`);
         document.querySelector('.weather_city').innerHTML = `${insertData.name}, ${insertData.sys.country}`;
         document.querySelector('.weather_temp').innerHTML = Math.round(insertData.main.temp) + `&degC`;
         document.querySelector('.weather_description').textContent = insertData.weather[0].description;
-        document.querySelector('.weather_icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${insertData.weather[0].icon}@2x.png">`;
         document.querySelector('.wind_speed').innerHTML = `${Math.round(insertData.wind.speed)}m/s`
         document.querySelector('.humidity').innerHTML = `${Math.round(insertData.main.humidity)}%`
         document.querySelector('.weather-info').style.display = 'flex';
         document.querySelector('.error_data').style.display = 'none'
+        document.querySelector('.weather_max').innerHTML = `max temp = ${insertData.main.temp_max}&degC`;
+        document.querySelector('.weather_feels').innerHTML = `feels like = ${insertData.main.feels_like}&degC`;
+
     } else if (insertData.cod !== 200) {
         document.querySelector('.error_data').style.display = 'block'
     }
+
+    setImg(weatherCartIcon);
 }
 
 searchCityBtn.addEventListener("click", () => {
@@ -34,6 +40,10 @@ searchCityBtn.addEventListener("click", () => {
 })
 
 
-
-
-console.log(window.innerWidth)
+function setImg(icon) {
+    let iconAttr = icon.getAttribute('src')
+    console.log(iconAttr)
+    if (iconAttr.includes('01n')) {
+        document.querySelector('.main_section').style.backgroundImage = `url(https://openweathermap.org/img/wn/01n@2x.png)`
+    }
+}
